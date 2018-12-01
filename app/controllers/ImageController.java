7d9e8382cc91e3b021b02f78b9729d597cc2f69f
Lucas Weidner <weidnerl@gmx.de>
2018-12-01 01:45:03 +0100

    First commit

---
+++ app/controllers/ImageController.java
@@ -0,0 +1,30 @@
+package controllers;
+
+import services.Counter;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.util.Calendar;
+import java.text.SimpleDateFormat;
+import play.mvc.*;
+import views.html.*;
+
+/**
+ * This controller demonstrates how to use dependency injection to
+ * bind a component into a controller class. The class contains an
+ * action that shows an incrementing count to users. The {@link Counter}
+ * object is injected by the Guice dependency injection system.
+ */
+@Singleton
+public class ImageController extends Controller {
+
+    @Inject
+    public ImageController(Counter counter) {}
+
+    public Result showImage(int day) {
+  
+		Calendar today = Calendar.getInstance();
+		if(today.get(Calendar.DATE) >= day)
+			return ok(image.render(day));
+		return ok(calendar.render());
+    }
+}
